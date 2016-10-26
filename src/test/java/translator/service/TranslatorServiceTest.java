f6fbc6f997e64484666cab9d80ef427e6ae90d84
juan-vg <632821@unizar.es>
2016-10-27 00:11:42 +0200

    Added new tests
    - Test translation from English to Urdu
    
    - Test translation from English to Swahili

--- src/test/java/translator/service/TranslatorServiceTest.java
+++ src/test/java/translator/service/TranslatorServiceTest.java
@@ -23,5 +23,23 @@ public class TranslatorServiceTest {
         TranslatedText translatedText = translatorService.translate("en", "es", "This is a test of translation service");
         assertEquals("Esto es una prueba de servicio de traducción",translatedText.getTranslation());
     }
-
+    
+    
+    @Test
+    /**
+     * Test translation from English to Urdu 
+     */
+    public void translateTestUrdu() throws Exception {
+        TranslatedText translatedText = translatorService.translate("en", "ur", "This is a test of translation service");
+        assertEquals("یہ ایک ٹیسٹ کے ترجمہ کی خدمت",translatedText.getTranslation());
+    }
+    
+    @Test
+    /**
+     * Test translation from English to Swahili
+     */
+    public void translateTestSwahili() throws Exception {
+        TranslatedText translatedText = translatorService.translate("en", "sw", "This is a test of translation service");
+        assertEquals("Hii ni mtihani wa tafsiri huduma",translatedText.getTranslation());
+    }
 }
